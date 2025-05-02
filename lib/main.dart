a350620db7e7eee4b744ebb83dcfe0ee459d869e
NgocNguyen1103 <quangquangngoc@gmail.com>
2025-05-03 00:24:56 +0700

    setstage

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+//String? tille = "Flutter A"; //String? :this value can be null
 
-//stateless: screen not change
-//material app
-//scaffold: skeleton of the app
+// stateless: screen cannot refresh
+// stateful: screen can refresh
+// setstate: to refresh the screen
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,40 +17,48 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //theme opf application
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text("Flutter App"), centerTitle: true),
-        drawer:
-        SafeArea(child:Drawer(
-          child: ListTile(title: Text("Logout"),),
-        ),
-        ),
-
-
-        floatingActionButton: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
-            SizedBox(height: 10.0),
-            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
-          ],
-        ),
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-            NavigationDestination(icon: Icon(Icons.settings), label: "Setting"),
-          ],
-          onDestinationSelected: (int value) {
-            print(value);
-          },
-          selectedIndex: 0,
-        ),
-      ),
 
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.teal,
           brightness: Brightness.dark,
         ),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int curentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Flutter App"), centerTitle: true),
+      body:
+          curentIndex == 0
+              ? Center(child: Text("1"))
+              : Center(child: Text("2")),
+      //tille!: ennsure the variable is not null
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.settings), label: "Setting"),
+        ],
+        onDestinationSelected: (int value) {
+          setState(() {
+            curentIndex = value;
+          });
+        },
+        selectedIndex: curentIndex,
       ),
     );
   }
