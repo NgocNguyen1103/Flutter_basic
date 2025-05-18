3d0679b3f1833245f6b047aaf6522ab8c2e66db6
NgocNguyen1103 <quangquangngoc@gmail.com>
2025-05-18 11:42:35 +0700

    checkbox and switch

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -9,21 +9,61 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   TextEditingController controller = TextEditingController();
+  bool? isChecked = false;
+  bool isSwitch = false;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(10.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           TextField(
             controller: controller,
             decoration: InputDecoration(border: UnderlineInputBorder()),
-            onEditingComplete: () {
-              setState(() {});
+            onEditingComplete: () => setState(() {}),
+          ),
+          //
+          Checkbox(
+            tristate: true,
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value;
+                print(value);
+              });
             },
           ),
-          Text(controller.text),
+          CheckboxListTile(
+            tristate: true,
+            title: Text("CheckboxListTile"),
+            subtitle: Text("subtitle"),
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value;
+              });
+            },
+          ),
+          Switch(
+            value: isSwitch,
+            onChanged: (bool value) {
+              setState(() {
+                isSwitch = value;
+              });
+            },
+          ),
+          SwitchListTile(
+            title: Text("SwitchListTile"),
+            subtitle: Text("subtitle"),
+            value: isSwitch,
+            onChanged: (bool value) {
+              setState(() {
+                isSwitch = value;
+              });
+            },
+          ),
         ],
       ),
     );
