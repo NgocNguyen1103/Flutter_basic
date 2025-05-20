ad977f428bc96216047ce9d9d8e8ca95a6b8c51d
NgocNguyen1103 <quangquangngoc@gmail.com>
2025-05-20 22:29:23 +0700

    constaint style

---
+++ lib/data/Constants.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+class KTextStyle{
+  static const TextStyle titleTealText = TextStyle(
+      fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.teal
+  );
+  static const TextStyle descriptionText = TextStyle(
+    fontSize: 14.0
+  );
+}
--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_one/data/Constants.dart';
 import 'package:flutter_one/views/widgets/hero_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,7 +9,27 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(20.0),
-      child: Column(children: [HeroWidget()]),
+      child: Column(
+        children: [
+          HeroWidget(),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 20.0),
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Card", style: KTextStyle.titleTealText),
+                    Text("Description of this card", style: KTextStyle.descriptionText),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
